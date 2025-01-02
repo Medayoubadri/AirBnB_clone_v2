82e184bf64425d76d7ca36723a83f7811303c170
medayoubadri <medayoubadri@gmail.com>
2025-01-02 19:42:38 +0100

    6. DBStorage - States and Cities

--- models/base_model.py
+++ models/base_model.py
@@ -44,10 +44,9 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models import storage
         self.updated_at = datetime.now()
-        storage.new(self)
-        storage.save()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
@@ -57,11 +56,10 @@ class BaseModel:
             {'__class__': (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-        if '_sa_instance_state' in dictionary:
+        if '_sa_instance_state' in dictionary.keys():
             del dictionary['_sa_instance_state']
         return dictionary
 
     def delete(self):
         """Delete the current instance from the storage"""
-        from models import storage
-        storage.delete(self)
+        models.storage.delete(self)
--- models/state.py
+++ models/state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from os import getenv
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
@@ -10,14 +11,15 @@ import models
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship(
-        "City",
-        backref="state",
-        cascade="all, delete-orphan"
-        )
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship(
+            "City",
+            backref="state",
+            cascade="all, delete-orphan"
+            )
 
-    if models.storage_t != "db":
+    else:
         @property
         def cities(self):
             """getter for list of city instances related to the state"""
