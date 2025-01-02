c313f95a65af6c331e53db8e7469b63d589f006b
medayoubadri <medayoubadri@gmail.com>
2025-01-02 19:52:28 +0100

    7. DBStorage - User

--- models/state.py
+++ models/state.py
@@ -16,7 +16,7 @@ class State(BaseModel, Base):
         cities = relationship(
             "City",
             backref="state",
-            cascade="all, delete-orphan"
+            cascade="all, delete"
             )
 
     else:
