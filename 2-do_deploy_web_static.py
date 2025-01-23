8bca0dc1f640185f30e37125cdc70f1ea0359c68
medayoubadri <medayoubadri@gmail.com>
2025-01-23 18:50:09 +0100

    Task 2. Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,50 +1,34 @@
 #!/usr/bin/python3
 """
-Fabric script that distributes an archive to the web servers.
+Fabric script that distributes an archive to the web servers
 """
 
 from fabric.api import env, put, run
 from os.path import exists
 
+
 env.hosts = ['34.224.63.237', '52.86.3.5']
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to the web servers."""
+    """Distributes an archive to the web servers"""
     if not exists(archive_path):
         return False
 
     try:
+
         file_name = archive_path.split("/")[-1]
         folder_name = file_name.split(".")[0]
-        path = f"/data/web_static/releases/{folder_name}"
+        path = "/data/web_static/releases/"
 
-        if put(archive_path, f"/tmp/{file_name}").failed:
-            return False
-
-        if run(f"mkdir -p {path}").failed:
-            return False
-
-        if run(f"tar -xzf /tmp/{file_name} -C {path}").failed:
-            return False
-
-        if run(f"rm /tmp/{file_name}").failed:
-            return False
-
-        if run(f"mv {path}/web_static/* {path}/").failed:
-            return False
-
-        if run(f"rm -rf {path}/web_static").failed:
-            return False
-
-        if run("rm -rf /data/web_static/current").failed:
-            return False
-
-        if run(f"ln -s {path} /data/web_static/current").failed:
-            return False
-
-        print("New version deployed!")
+        put(archive_path, "/tmp/")
+        run(f"mkdir -p {path}{folder_name}/")
+        run(f"tar -xzf /tmp/{file_name} -C {path}{folder_name}/")
+        run(f"rm /tmp/{file_name}")
+        run(f"mv {path}{folder_name}/web_static/* {path}{folder_name}/")
+        run(f"rm -rf {path}{folder_name}/web_static")
+        run("rm -rf /data/web_static/current")
+        run(f"ln -s {path}{folder_name}/ /data/web_static/current")
         return True
-    except Exception as e:
-        print(f"Deployment failed: {e}")
+    except Exception:
         return False
