decd5d5ddf0d6319df6f6374225c729db23a6ce0
medayoubadri <medayoubadri@gmail.com>
2025-01-26 12:29:33 +0100

    Task 4. Keep it clean-O-h

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -3,7 +3,7 @@
 Fabric script that deletes out-of-date archives
 """
 
-from fabric.api import env, lcd, cd, local, run
+from fabric.api import env, cd, lcd, local, run
 import os
 
 env.hosts = ['34.224.63.237', '52.86.3.5']
@@ -18,19 +18,21 @@ def do_clean(number=0):
         If number is 2, keeps the most and second most recent archives, etc.
     """
     number = int(number)
-    if number < 0:
-        return False
-    number = max(1, number) if number == 0 else number + 1
+    if number < 1:
+        number = 1
 
-    local_archives = local('ls -1t versions', capture=True).split()
-    for archive in local_archives[number:]:
-        local(f'rm -rf versions/{archive}')
+    local_path = "versions"
+    if os.path.isdir(local_path):
+        with lcd(local_path):
+            archives = sorted(local("ls -1t", capture=True).split())
+            [local(f"rm -f {archive}") for archive in archives[number:]]
 
-    releases_path = '/data/web_static/releases'
-    remote_archives = run(f'ls -1t {releases_path}').split()
-    web_static_dirs = [d for d in remote_archives
-                       if d.startswith('web_static_')]
-    for archive in web_static_dirs[number:]:
-        run(f'rm -rf {releases_path}/{archive}')
-
-    return True
+    with cd("/data/web_static/releases"):
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if a.startswith("web_static_")]
+        if number <= 0 or number >= len(archives):
+            pass
+        else:
+            archives_to_delete = archives[:-number]
+            for archive in archives_to_delete:
+                run(f"rm -rf {archive}")
