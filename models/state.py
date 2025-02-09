b2a839cc1f7c086e7da4d6e1df5ef98a1f558bcd
medayoubadri <medayoubadri@gmail.com>
2025-02-09 18:43:37 +0100

    Task 7. Improve engines

--- models/state.py
+++ models/state.py
@@ -3,23 +3,19 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.city import City
-import models
-
+from os import getenv
 
 class State(BaseModel, Base):
-    """ State class """
-    if models.storage_type == "db":
-        __tablename__ = 'states'
-        name = Column(String(128), nullable=False)
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
         cities = relationship("City", backref="state", cascade="all, delete")
     else:
         @property
         def cities(self):
             """getter for list of city instances related to the state"""
-            city_list = []
-            all_cities = models.storage.all(City)
-            for city in all_cities.values():
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+            from models import storage
+            from models.city import City
+            return [city for city in storage.all(City).values() 
+                    if city.state_id == self.id]
