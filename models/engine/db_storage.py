cde1d05bc3025f4fbb25f6a4a564d45caff19918
medayoubadri <medayoubadri@gmail.com>
2025-02-10 10:27:55 +0100

    Fixing bugs

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -65,9 +65,9 @@ class DBStorage:
         """Reloads data from the database"""
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(
-                  bind=self.__engine, expire_on_commit=False)
+            bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(session_factory)
-        self.__session = Session
+        self.__session = Session()
 
     def close(self):
         """call remove() method on the private session attribute"""
--- models/place.py
+++ models/place.py
@@ -30,6 +30,7 @@ class Place(BaseModel, Base):
     """Representation of Place """
     if models.storage_type == 'db':
         __tablename__ = 'places'
+        id = Column(String(60), primary_key=True)
         city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         name = Column(String(128), nullable=False)
